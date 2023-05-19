15dea0215534658bcaf9b40f06e19d7d0df36261
Sudip Roy <sudiproy1999@gmail.com>
2023-05-19 22:36:16 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { Login } from './components/Login';
 
 import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <GlobalProvider>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              {!isLoggedIn ? (
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              ) : (
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              {isLoggedIn ? (
+                <>
+                  <Header />
+                  <div className="container">
+                    <Balance />
+                    <IncomeExpenses />
+                    <TransactionList />
+                    <AddTransaction />
+                  </div>
+                </>
+              ) : (
+                <h2>Please login to view the content</h2>
+              )}
+            </Route>
+            <Route path="/login">
+              <Login handleLogin={handleLogin} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </GlobalProvider>
   );
 }
 
 export default App;
+S
